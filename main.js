9e31c2587d9c1fdb93c51fdeece81c2e373d2601
AaryaCoder <84276676+AaryaCoder@users.noreply.github.com>
2021-11-28 09:35:55 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -16,8 +16,25 @@ function take_snapshot(){
 }
 
 console.log('ml5 version:',ml5.version);
-classifier=ml5.imageClassifier('https://storage.googleapis.com/tm-model/uc4BrxmDB/model.json',modelLoaded);
+classifier=ml5.imageClassifier('https://storage.googleapis.com/tm-model/bIA87liF5/model.json',modelLoaded);
 
 function modelLoaded(){
     console.log('model loaded!');
+}
+
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
 }
